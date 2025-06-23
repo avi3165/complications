a3893a1c11204bf635ebc95aa28abd80758a8e3d
avi3165 <avi316532068@gmail.com>
2025-06-23 12:18:58 +0300

    add q7-q9

--- complications_1.py
+++ complications_1.py
@@ -28,4 +28,34 @@
 # for i,v in enumerate(list_2):
 #     if i<len(list_2)/2:
 #         print(v)
-#בגלל שהרשימה ידועה מראש אז זוהי סיבוכיות קבועה כי היא ידועה מראש
+#בגלל שהרשימה משתנה אז זוהי סיבוכיות לינארית כי היא נתונה מהמשתמש
+#7
+# list_3=[3,7,23,46,89,61,2,72,11]
+# def find_element(n:int):
+#     a = False
+#     for i,v in enumerate(list_3):
+#         if n==v:
+#             a=True
+#             print("the number found in index",i)
+#     if a ==False:
+#         print("The number was not found")
+# n=12
+#סיבוכיות לניארית כי זה תלוי בגודל הרשימה
+#8
+# def find_even(li:list[int]):
+#     for v in li:
+#         if v%2==0:
+#             print(v)
+# find_even([2,3,7,6,4,12,14])
+#סיבוכיות לניארית היות ותלויה באורך רשימה
+#9
+def max_number(li:str):
+    l=li.split(',')
+    l_1=[]
+    for i in l:
+        l_1.append(int(i))
+    a=max(l)
+    print(a)
+li=(input("Please enter a list of numbers: "))
+max_number(li)
+#סיבוכיות לניארית מפני שיש צורך בקלט וכמות הפעולות משתנה בהתאם לגודל הקלט
